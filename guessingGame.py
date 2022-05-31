89fe1c247266a4c12f74082cef2dbb4c3b984fa6
Micah Potts <mrs.micahpotts@gmail.com>
2022-05-31 19:43:14 -0400

    formatting

--- guessingGame.py
+++ guessingGame.py
@@ -2,38 +2,45 @@
 
 import random
 
-play = True
+def main():
+    play = True
 
-while play:
+    while play:
 
-    guessLimit = 0
+        guessLimit = 0
 
-    topOut = input("Pick a number.")
-    topOut = int(topOut)
+        topOut = input("Pick a number.\n")
+        topOut = int(topOut)
 
-    rando = random.randint(1, topOut)
+        rando = random.randint(1, topOut)
 
-    print("Guess a number between 1 and " + str(topOut) + " and I will tell you if it is too high or too low.")
+        print(
+            "Guess a number between 1 and "
+            + str(topOut)
+            + " and I will tell you if it is too high or too low."
+        )
 
-    while(guessLimit < topOut):
-        guess = int(input())
+        while guessLimit < topOut:
+            guess = int(input())
 
-        guessLimit = guessLimit + 1
-        
-        if (rando > guess):
-            print("You're too low, try again!")
-        elif (rando < guess):
-            print("You're too high, try again!")
-        else: 
-            print("You're the best! That's it!")
-            again = raw_input("Want to play again? (y/n)")
-            if(again == "y"):
-                play == True
-                break
+            guessLimit = guessLimit + 1
+
+            if rando > guess:
+                print("You're too low, try again!")
+            elif rando < guess:
+                print("You're too high, try again!")
             else:
-                play == False
-                exit()
-    else:
-        print("You guessed " + str(topOut) + " times. Start again.")
-    
+                print("You're the best! That's it!")
+                again = input("Want to play again? (y/n)")
+                if again == "y":
+                    play == True
+                    break
+                else:
+                    play == False
+                    exit()
+        else:
+            print("You guessed " + str(topOut) + " times. Start again.")
 
+
+if __name__ == "__main__":
+    main()
